ec25478e47bdb6bdcd1128d748b55600679045d2
minaix32176-jpg <minaix32176@gmail.com>
2026-03-12 17:36:52 +0900

    NAMG-006 : [Task] Implement player movement logic

--- Assets/Scripts/Actor/PlayerController.cs
+++ Assets/Scripts/Actor/PlayerController.cs
@@ -2,15 +2,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Movement Settings")]
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float rotationSpeed = 10f;
+
+    private Rigidbody rb;
+    private PlayerInputHandler inputHandler;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        inputHandler = GetComponent<PlayerInputHandler>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        ApplyMovement();
+    }
+
+    private void ApplyMovement()
+    {
+        Vector3 moveDirection = new Vector3(inputHandler.moveInput.x, 0, inputHandler.moveInput.y);
+
+        Vector3 targetVelocity = moveDirection * speed;
+        rb.linearVelocity = new Vector3(targetVelocity.x, rb.linearVelocity.y, targetVelocity.z);
+
+        if (moveDirection.magnitude > 0.1f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+            rb.MoveRotation(Quaternion.Slerp(rb.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime));
+        }
     }
 }
---
+++ Assets/Scripts/Actor/PlayerInputHandler.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    public Vector2 moveInput; 
+
+    private void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+}
