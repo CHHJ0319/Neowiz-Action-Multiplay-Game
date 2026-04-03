4b44da1d603ad41be19c9edfa56065c60a7e25c8
jjahoo5116-lang <jjahoo5116@gmail.com>
2026-04-03 17:46:19 +0900

    NAMG-116 : [Task] Implement an enemy audio handler

---
+++ Assets/Scripts/Actor/Enemy/EnemyAudioHandler.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+namespace Actor.Enemy
+{
+    public class EnemyAudioHandler : MonoBehaviour
+    {
+        private AudioSource _audioSource;
+
+        [Header("Audio Clips")]
+        public AudioClip crySound;  
+        public AudioClip deathSound; 
+
+        private void Awake()
+        {
+            _audioSource = GetComponent<AudioSource>();
+        }
+
+        public void PlayCrySound()
+        {
+            PlaySFX(crySound);
+        }
+
+        public void PlayDeathSound()
+        {
+            PlaySFX(deathSound);
+        }
+
+        private void PlaySFX(AudioClip clip)
+        {
+            if (clip != null && _audioSource != null)
+            {
+                _audioSource.PlayOneShot(clip);
+            }
+        }
+    }
+}
