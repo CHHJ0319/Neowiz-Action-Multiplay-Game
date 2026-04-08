a1e61fc164b09650b928dd012ee4cbaec9401d33
CHHJ0319 <83933117+CHHJ0319@users.noreply.github.com>
2026-04-08 17:46:31 +0900

    NAMG-131 : [Task] Implement EnemyInfo struct

---
+++ Assets/Scripts/Data/EnemyInfo.cs
@@ -0,0 +1,14 @@
+namespace Data
+{
+    public struct EnemyInfo
+    {
+        public EnemyType type;
+        public int lives;
+    }
+
+    public enum EnemyType
+    {
+        Single, Multy
+    }
+}
+
