5faa22355b6b23fe09e5477d6078304bc54989cd
jjahoo5116-lang <jjahoo5116@gmail.com>
2026-04-09 17:29:00 +0900

    NAMG-148 : [Task] Implement Session Menu Button

---
+++ Assets/Scripts/UI/LobbyScene/SessionMenuButton.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SessionMenuButton : MonoBehaviour
+{
+    [Header("Individual Sound")]
+    public AudioClip clickSound;
+
+    private void Awake()
+    {
+        Button btn = GetComponent<Button>();
+
+        if (btn != null)
+        {
+            btn.onClick.AddListener(() => PlayClickSound());
+        }
+    }
+
+    private void PlayClickSound()
+    {
+        if (clickSound != null)
+        {
+            Vector3 cameraPos = Camera.main.transform.position;
+            AudioSource.PlayClipAtPoint(clickSound, cameraPos);
+        }
+    }
+}
