e6f6ff4d536eb22081ea0d68f8806fb6a0f2a7f0
CHHJ0319 <83933117+CHHJ0319@users.noreply.github.com>
2026-04-13 12:05:45 +0900

    NAMG-162 : [Task] Implement seamless looping for prologue text scroll

--- Assets/Scripts/UI/PrologueScene/ProloguePanel.cs
+++ Assets/Scripts/UI/PrologueScene/ProloguePanel.cs
@@ -1,15 +1,40 @@
 using UnityEngine;
 
-public class ProloguePanel : MonoBehaviour
+namespace UI.PrologueScene
 {
-    public RectTransform prologueText;
-    public float scrollSpeed = 30f;
-
-    private void Update()
+    public class ProloguePanel : MonoBehaviour
     {
-        if (prologueText != null)
+        public RectTransform prologueMask;
+        public RectTransform prologueText;
+        public float scrollSpeed = 30f;
+
+        private Vector2 startPosition;
+        private float resetThreshold;
+
+        private void Start()
         {
-            prologueText.anchoredPosition += Vector2.up * scrollSpeed * Time.deltaTime;
+            if(prologueText != null && prologueMask != null)
+        {
+                float maskHeight = prologueMask.rect.height;
+                float textHeight = prologueText.rect.height;
+
+                startPosition = new Vector2(prologueText.anchoredPosition.x, prologueText.anchoredPosition.y);
+
+                resetThreshold = maskHeight + textHeight / 2f;
+            }
+        }
+
+        private void Update()
+        {
+            if (prologueText != null)
+            {
+                prologueText.anchoredPosition += Vector2.up * scrollSpeed * Time.deltaTime;
+
+                if (prologueText.anchoredPosition.y > resetThreshold)
+                {
+                    prologueText.anchoredPosition = startPosition;
+                }
+            }
         }
     }
 }
