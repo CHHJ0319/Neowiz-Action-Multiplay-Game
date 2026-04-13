2f0739e977d92d73fa30abcff46bb460f4b6dafe
CHHJ0319 <83933117+CHHJ0319@users.noreply.github.com>
2026-04-13 16:33:25 +0900

    Implement PingItem

---
+++ Assets/Scripts/Data/RequestData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Data
+{
+    public static class RequestData
+    {
+        public static List<string> MessageList = new List<string>
+        {
+            "포션이 부족해!",
+            "상자 좀 치워줘!"
+        };
+    }
+}
