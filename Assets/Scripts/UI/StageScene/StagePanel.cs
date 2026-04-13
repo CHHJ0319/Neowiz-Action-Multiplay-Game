aecc59993a881565a7276fd84f0f68f8037957ee
CHHJ0319 <83933117+CHHJ0319@users.noreply.github.com>
2026-04-13 15:26:29 +0900

    Add StagePanel to the UI

---
+++ Assets/Scripts/UI/StageScene/StagePanel.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace UI.StageScene
+{
+    public class StagePanel : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
